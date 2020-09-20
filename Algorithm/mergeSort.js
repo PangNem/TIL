6892d282868548d92e186dc121e9a8a53f7f77c4
pangnem <sprnd645@gmail.com>
2020-09-20 21:14:50 +0900

    Algorithm - mergeSort, dynamic programming

---
+++ Algorithm/mergeSort.js
@@ -0,0 +1,27 @@
+const mergeSort = (array) => {
+  if (array.length < 2) return array;
+
+  const pivot = Math.floor(array.length / 2);
+  const left = array.slice(0, pivot);
+  const right = array.slice(pivot, array.length);
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+function merge(left, right) {
+  let result = [];
+
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+
+  while (left.length) result.push(left.shift());
+  while (right.length) result.push(right.shift());
+
+  return result;
+}
+
+mergeSort([5, 2, 4, 7, 6, 8, 1, 3]);
