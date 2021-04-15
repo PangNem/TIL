6855332cd5a23c4dc6f3da96d88663b8a3fc65c2
pangnem <sprnd645@gmail.com>
2021-04-15 23:28:58 +0900

    Java Lambda, Functional Interface

---
+++ Java/whiteship-java-study/Lambda.java
@@ -0,0 +1,45 @@
+public class Lambda {
+  int max(int a, int b) {
+    return a > b ? a : b;
+  }
+  // (int a, int b) -> a > b ? a : b;
+
+
+  void printVar(String name, int i) {
+    System.out.println(name+"="+i);
+  }
+
+  // (String name, int i) -> System.out.println(name+"="+i)
+
+  int square(int x) {
+    return x * x;
+  }
+  // (int x) -> {return x * x}
+  // (x) -> x * x
+  
+  int roll() {
+    return (int) (Math.random() * 6);
+  }
+  // () -> (int) (Math.random() * 6);
+
+  int sumArr(int[] arr) {
+    int sum = 0;
+    for (int i : arr) 
+      sum += i;
+    return sum;
+  }
+  
+  /*
+  int[] arr -> {
+    int sum = 0;
+    for (int i : arr)
+      sum += i;
+    return sum;
+  }
+  */
+
+}
+
+
+
+
